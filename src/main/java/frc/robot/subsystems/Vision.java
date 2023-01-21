2ac2d05ad78824bd80ebc4aa953444a4204f038e
SadiqAhmed <ahmedsadiq@apps.isd623.org>
2023-01-20 21:19:27 -0600

    Updated Photon + Pose Estimation

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -4,16 +4,28 @@
 
 package frc.robot.subsystems;
 
+import java.io.IOException;
 import java.lang.annotation.Target;
+import java.lang.reflect.Field;
 import java.text.Format;
+import java.util.Optional;
 
 import org.ejml.dense.block.VectorOps_FDRB;
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.RobotPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.apriltag.AprilTagPoseEstimator;
 import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -65,7 +77,6 @@ public class Vision extends SubsystemBase {
       }
     }
   }
-
   private class VisionMap {
     private VisionTarget[] Targets = new VisionTarget[8];
     private int LastSeen;
@@ -112,10 +123,24 @@ public class Vision extends SubsystemBase {
   PhotonCamera Camera;
   VisionMap VM;
 
+  PhotonPoseEstimator poseEstimator;
+  Field2d Field = new Field2d();
+  AprilTagFieldLayout layout;
+
   /** Creates a new Vision. */
   public Vision(String CamName) {
     Camera = new PhotonCamera(CamName);
     VM = new VisionMap();
+    SmartDashboard.putData("VisionEyes",Field);
+    
+    try {
+      layout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
+      poseEstimator = new PhotonPoseEstimator(layout, PoseStrategy.CLOSEST_TO_CAMERA_HEIGHT, Camera, new Transform3d());
+      
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 
   @Override
@@ -127,6 +152,13 @@ public class Vision extends SubsystemBase {
     PhotonPipelineResult result = Camera.getLatestResult();
     if(result.hasTargets()){
       VM.onVisionTargetSeen(result.getBestTarget());
+      Optional<EstimatedRobotPose> pose = poseEstimator.update();
+      if(pose.isPresent()){
+        //Pose is valid
+        Pose3d p3d = pose.get().estimatedPose;
+        //SmartDashboard.putString("VisionEayes",("" + p3d.toPose2d().getX() + ',' + p3d.toPose2d().getY()));
+        Field.setRobotPose(p3d.toPose2d());
+      }
     }
   }
 }
